912dbbd205cf1e58282d8ace35afa0c19d56163b
Bryce Wendelaar <brycewendelaar@InteraXons-MacBook-Pro.local>
2017-09-01 15:01:25 -0400

    Added paths for adding new client

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :invoices do
     resources :lines
   end
+  root to: "invoices#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
