1e2d7814882b2ed033918c20104e62154c903b91
Wendelcorp <bryce@brycewendelaar.com>
2017-09-05 22:18:22 -0400

    Update invoice payment status after payment

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,20 +3,24 @@ class ChargesController < ApplicationController
 end
 
 def create
+  @amount = params[:amount]
+  @invoice = Invoice.find(params[:invoice_id])
+  @invoice.update(status: "true")
+  @invoice.save
 
   customer = Stripe::Customer.create(
     :email => params[:stripeEmail],
     :source  => params[:stripeToken]
   )
-
   @stripe_account = current_user.uid
 
   charge = Stripe::Charge.create({
     :currency => "cad",
     :source => "tok_visa",
-    :amount => params[:amount],
+    :amount => @amount,
     :application_fee => 500,
   }, :stripe_account => current_user.uid)
+
 
 rescue Stripe::CardError => e
   flash[:error] = e.message
