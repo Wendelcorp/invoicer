adcc9306c6a13095729fffdf819945d1a6797bab
Wendelcorp <bryce@brycewendelaar.com>
2017-09-10 23:39:54 -0400

    Fixed bug with connected stripe user account

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -7,19 +7,20 @@ def create
   @invoice = Invoice.find(params[:invoice_id])
   @invoice.update(status: "true")
   @invoice.save
+  @user = User.find(@invoice.user_id)
 
   customer = Stripe::Customer.create(
     :email => params[:stripeEmail],
     :source  => params[:stripeToken]
   )
-  @stripe_account = current_user.uid
+  @stripe_account = @user.uid
 
   charge = Stripe::Charge.create({
     :currency => "cad",
     :source => "tok_visa",
     :amount => @amount,
     :application_fee => 500,
-  }, :stripe_account => current_user.uid)
+  }, :stripe_account => @user.uid)
 
 
 rescue Stripe::CardError => e
