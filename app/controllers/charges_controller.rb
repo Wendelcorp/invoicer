9d49e83717c8a0dd5d40e8b4185339a17b87306e
Bryce Wendelaar <brycewendelaar@InteraXons-MacBook-Pro.local>
2017-09-27 16:34:20 -0400

    Front end styling

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,29 +2,30 @@ class ChargesController < ApplicationController
   def new
 end
 
-def create
-  @amount = params[:amount]
-  @invoice = Invoice.find(params[:invoice_id])
-  @invoice.update(status: "true")
-  @invoice.save
-  @user = User.find(@invoice.user_id)
+  def create
+    @amount = params[:amount]
+    @invoice = Invoice.find(params[:invoice_id])
+    @invoice.update(status: "true")
+    @invoice.save
+    @user = User.find(@invoice.user_id)
 
-  customer = Stripe::Customer.create(
-    :email => params[:stripeEmail],
-    :source  => params[:stripeToken]
-  )
-  @stripe_account = @user.uid
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+    @stripe_account = @user.uid
 
-  charge = Stripe::Charge.create({
-    :currency => "cad",
-    :source => "tok_visa",
-    :amount => @amount,
-    :application_fee => 500,
-  }, :stripe_account => @user.uid)
+    charge = Stripe::Charge.create({
+      :currency => "cad",
+      :source => "tok_visa",
+      :amount => @amount,
+      :application_fee => 500,
+    }, :stripe_account => @user.uid)
 
 
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_charge_path
-end
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+
 end
